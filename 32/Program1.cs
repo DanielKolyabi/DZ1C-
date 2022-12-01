26cfedbaa901e0091e467845339bc717491c5c79
Денис Колябин <Kolabin14.@gmail.com>
2022-12-01 19:18:50 +0300

    DZ

--- 32/Program1.cs
+++ 32/Program1.cs
@@ -1,7 +1,7 @@
 Console.WriteLine("Введите 1 число!");
 int a = Convert.ToInt32(Console.ReadLine());
 int s;
-while (a < 99 && a > 1000 );
+while (a < 99 && a > 1000 )
 {
 s = (a / 10) % 10;
 Console.WriteLine($"Вторая цифра числа {s}! ");
