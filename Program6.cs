2bb72675a9e2319148401ee8ec0b9f76d469b53b
Денис Колябин <Kolabin14.@gmail.com>
2022-12-01 21:02:42 +0300

    DZ

--- Program6.cs
+++
@@ -1,6 +0,0 @@
-// Console.WriteLine("Введите одно число!");
-// int a = Convert.ToInt32(Console.ReadLine());
-// if (a % 2 == 0)
-// Console.WriteLine("да!");
-// else 
-// Console.WriteLine("Нет!");
--- Program8.cs
+++
@@ -1,9 +0,0 @@
-Console.WriteLine("Введите одно число!");
-int n = Convert.ToInt32(Console.ReadLine());
-int size = 0;
-while(size<n)
-{
-    Console.Write($"{size} ");
-    size = size+2;
-
-}
