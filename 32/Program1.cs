aea31fda14325efdcc276f437a547395994bccc6
Денис Колябин <Kolabin14.@gmail.com>
2022-12-01 19:16:13 +0300

    DZ

---
+++ 32/Program1.cs
@@ -0,0 +1,9 @@
+Console.WriteLine("Введите 1 число!");
+int a = Convert.ToInt32(Console.ReadLine());
+int s;
+while (a < 99 && a > 1000 );
+{
+s = (a / 10) % 10;
+Console.WriteLine($"Вторая цифра числа {s}! ");
+}
+Console.WriteLine("Введино некоректное число!");
