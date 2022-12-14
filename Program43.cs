334c3beeb75d414fc0725df3927fe1640e2602f5
Денис Колябин <Kolabin14.@gmail.com>
2022-12-14 23:25:19 +0300

    first commit

---
+++ Program43.cs
@@ -0,0 +1,21 @@
+Console.WriteLine("Введите размер массива");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите размер массива");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите размер массива");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите размер массива");
+double k2 = Convert.ToInt32(Console.ReadLine());
+static double Sum (double b1, double b2, double k1, double k2)
+{
+double x = (-b2 + b1)/(-k1 + k2);
+
+ return x ;
+}
+static double Sum2 (double b1, double b2,double k1, double k2)
+{
+double x = Sum(b1,  b2,  k1,  k2);
+double y = k2 * x + b2;
+ return y ;
+}
+Console.WriteLine($"b1={b1} k1={k1} b2={b2} k2={k2} -> ({Sum(b1,  b2,  k1,  k2)};{Sum2(b1,  b2,  k1,  k2)})");
